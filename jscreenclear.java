cfd1904124f13ea26fbf5c4e4719a4b44cfbc470
draumaz <77967471+draumaz@users.noreply.github.com>
2021-07-22 15:18:24 -0700

    Exception errors implemented

--- jscreenclear.java
+++ jscreenclear.java
@@ -1,16 +1,38 @@
 // draumaz/jscreenclear
 // BSD-3
 
+import java.io.IOException;
+
 class jscreenclear {
 	public static void clear() {
 		if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) { // When system is Windows
 			try {
-				new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor(); // Clear with cmd
-			} catch (Exception e) {}
+			new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor(); // Clear with cmd
+			} catch (InterruptedException ie) {
+				exceptions(1);
+			} catch (IOException ioe) {
+				exceptions(2);
+			}
 		} else { // When system is not Windows
 			try {
-				new ProcessBuilder("/bin/sh","-c","clear").inheritIO().start().waitFor(); // Clear with POSIX sh
-			} catch (Exception e) {}
+			new ProcessBuilder("/bin/sh","-c","clear").inheritIO().start().waitFor(); // Clear with POSIX sh
+			} catch (InterruptedException ie) {
+				exceptions(1);
+			} catch (IOException ioe) {
+				exceptions(2);
+			}
+		}
+	}
+	public static void exceptions(int which) {
+		switch (which) {
+			case 1:
+				System.out.println("Interrupted exception encountered.");
+				System.exit(0);
+				break;
+			case 2:
+				System.out.println("IO exception encountered.");
+				System.exit(0);
+				break;
 		}
 	}
 }
