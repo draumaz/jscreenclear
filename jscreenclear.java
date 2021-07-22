5558bd89e8f6d6b6017f7c0d958098550244e634
draumaz <77967471+draumaz@users.noreply.github.com>
2021-07-22 00:37:39 -0700

    Update jscreenclear.java

--- jscreenclear.java
+++ jscreenclear.java
@@ -1,9 +1,12 @@
+// draumaz/jscreenclear
+// BSD-3
+
 class jscreenclear {
 	public static void clear() {
 		if (System.getProperty("os.name").toLowerCase().indexOf("win") >= 0) { // When system is Windows
-		try {
-			new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor(); // Clear with cmd
-		} catch (Exception e) {}
+			try {
+				new ProcessBuilder("cmd","/c","cls").inheritIO().start().waitFor(); // Clear with cmd
+			} catch (Exception e) {}
 		} else { // When system is not Windows
 			try {
 				new ProcessBuilder("/bin/sh","-c","clear").inheritIO().start().waitFor(); // Clear with POSIX sh
